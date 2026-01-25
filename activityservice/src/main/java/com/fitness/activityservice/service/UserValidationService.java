4fcd498e63bcfd36b6c55809ac8e8c249e283a85
Berk Akgül <108866525+akgullberk@users.noreply.github.com>
2026-01-25 17:15:57 +0300

    setting up the AI Service

--- activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
 @Slf4j
